83512978f1aa69a09f85a6ea635dea3471e8c469
Alon Segal <anialonsegal@gmail.com>
2024-02-14 11:10:43 +0200

    Added a custom error that also returns the current url and status code

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const ResolveRedirectError = require('./ResloveRedirectError');
 const getResponse = require('./get-response');
 
 module.exports = resolveRedirect;
@@ -16,15 +17,15 @@ function resolveRedirect(url, maxRedirect, callback) {
   return getResponse(currentUrl)
     .then(function handleResponse(response) {
       let done;
-      if (response.statusCode === 404 && response.req.method === 'HEAD'){ // Some Amazon servers return 404 fot HEAD,so we try GET: https://stackoverflow.com/questions/73413321/unshort-amazon-eu-link-using-python
+      if (response.statusCode === 404 && response.req.method === 'HEAD') { // Some Amazon servers return 404 fot HEAD,so we try GET: https://stackoverflow.com/questions/73413321/unshort-amazon-eu-link-using-python
           return getResponse(currentUrl, null, 'GET').then( handleResponse )
       }
       else if (response.statusCode === 301 || response.statusCode === 302) {
         currentUrl = response.headers.location;
         count = count + 1;
-      } else if (response.statusCode < 200 || response.statusCode === 404 || response.statusCode>=500) {
-        throw new Error(`${currentUrl} returned HTTP ${response.statusCode}`)
-      }else {
+      } else if (response.statusCode < 200 || response.statusCode === 404 || response.statusCode >= 500) {
+        throw new ResolveRedirectError(`${currentUrl} returned HTTP ${response.statusCode}`, response.statusCode, currentUrl);
+      } else {
         done = true;
       }
 
